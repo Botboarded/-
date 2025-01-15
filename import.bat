e4e1e69c2434a7a22e34cd63c3e1f9c0da97bf74
Hugo Walton <159638962+Botboarded@users.noreply.github.com>
2025-01-15 07:32:44 +0000

    Add files via upload

---
+++ import.bat
@@ -0,0 +1,15 @@
+pip install pywin32
+
+pip install numpy
+
+pip install keybaord
+
+pip install pyautogui
+
+pip install pygame
+
+pip install homeassistant
+
+pip install flask
+
+pip install dash
